521ea8c20aa6ae6277bd7248bc01ee630f0a1a61
Mahmoud Zaher <mahmoudzaher.developer@gmail.com>
2023-11-06 19:06:43 +0300

    feat: call list summary each 8 seconds

--- app/src/main/java/com/example/cctaskofyahoofinance/presentation/ui/summarylist/MainActivityViewModel.kt
+++ app/src/main/java/com/example/cctaskofyahoofinance/presentation/ui/summarylist/MainActivityViewModel.kt
@@ -7,6 +7,7 @@ import androidx.lifecycle.viewModelScope
 import com.example.cctaskofyahoofinance.data.source.remote.Resource
 import com.example.cctaskofyahoofinance.domain.usecase.GetSummaryList
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.launchIn
 import kotlinx.coroutines.flow.onEach
 import kotlinx.coroutines.launch
@@ -26,35 +27,37 @@ class MainActivityViewModel @Inject constructor(
 
     private fun getSummaries() {
         viewModelScope.launch {
+            while (true) {
+                getSummaryList().onEach { result ->
+                    when (result) {
+                        is Resource.Loading -> {
+                            _state.value = state.value.copy(
+                                summaryList = result.data?.marketSummaryAndSparkResponse?.result
+                                    ?: emptyList(),
+                                isLoading = true
+                            )
+                        }
 
-            getSummaryList().onEach { result ->
-                when (result) {
-                    is Resource.Loading -> {
-                        _state.value = state.value.copy(
-                            summaryList = result.data?.marketSummaryAndSparkResponse?.result
-                                ?: emptyList(),
-                            isLoading = true
-                        )
+                        is Resource.Success -> {
+                            _state.value = state.value.copy(
+                                summaryList = result.data?.marketSummaryAndSparkResponse?.result
+                                    ?: emptyList(),
+                                isLoading = false
+                            )
+                        }
+
+                        is Resource.Error -> {
+                            _state.value = state.value.copy(
+                                summaryList = result.data?.marketSummaryAndSparkResponse?.result
+                                    ?: emptyList(),
+                                isLoading = false
+                            )
+
+                        }
                     }
-
-                    is Resource.Success -> {
-                        _state.value = state.value.copy(
-                            summaryList = result.data?.marketSummaryAndSparkResponse?.result
-                                ?: emptyList(),
-                            isLoading = false
-                        )
-                    }
-
-                    is Resource.Error -> {
-                        _state.value = state.value.copy(
-                            summaryList = result.data?.marketSummaryAndSparkResponse?.result
-                                ?: emptyList(),
-                            isLoading = false
-                        )
-
-                    }
-                }
-            }.launchIn(this)
+                }.launchIn(this)
+                delay(8000)
+            }
         }
     }
 }
